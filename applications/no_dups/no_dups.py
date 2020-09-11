303fbdc3335ba40dd548dd9840f15d6fadb853ab
DentonWarnock <dentonwarnock@gmail.com>
2020-09-10 21:09:16 -0700

    added no_dups.py

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,16 @@
 def no_dups(s):
     # Your code here
-
+    cache = {}
+    result = ""
+    # split string into an array of words
+    words = s.split()
+    # for each new word not in the cache add it to cache and result string
+    for word in words:
+        if word not in cache:
+            cache[word] = word
+            result += word + " " 
+    print(result)    
+    return result.rstrip() # removes trailing whitespace
 
 
 if __name__ == "__main__":
